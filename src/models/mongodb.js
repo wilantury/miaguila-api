ea8831de913816364f400446c3e441ff45ddfa7c
wilantury <ventas@antury.net>
2020-06-12 14:33:40 -0500

    [Add] update the status trip, and get a trip by id

--- src/models/mongodb.js
+++ src/models/mongodb.js
@@ -3,7 +3,7 @@
  *@module MONGODB
 */
 
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const config = require('../config/index');
 /**
  * Fetch the environment variables to connect to a data base Mongo.
@@ -27,13 +27,13 @@ connectMongo();
 
 /**
  * Get the quantity of documents into a collection.
- * @param {String} - city: the cityTarget to search into database
+ * @param {String} - query: the cityTarget to search into database
  * @param {String} - db: database target
  * @param {String} - collection: collection target
  * @returns {Promise} - Promise with the result.
  */
-async function getCountByCity(db, collection, cityTarget){
-  return await client.db(db).collection(collection).find({city:{name:cityTarget}}).count();
+async function getCountByCity(db, collection, query){
+  return await client.db(db).collection(collection).find(query).count();
 }
 
 /**
@@ -46,9 +46,39 @@ async function getCount(db, collection){
   return await client.db(db).collection(collection).find({}).count();
 }
 
+
+/**
+ * Get the a trip by id.
+ * @param {Object} - body with id
+ * @returns {Promise} - Promise with the result.
+ */
+function getTrip(db, collection, query){
+  return new Promise( (resolve, reject) => {
+    client.db(db).collection(collection).find(query).toArray((err, res) => {
+      if(err){
+        return reject(err);
+      }
+       resolve(res);
+    });
+  })
+}
+/**
+ * Update a trip of collection.
+ * @param {Object} - query
+ * @param {Object} - new values
+ * @param {String} - db: database target
+ * @param {String} - collection: collection target
+ * @returns {Promise} - Promise with the result.
+ */
+async function updateTrip(db, collection, query, newValues){
+  return await client.db(db).collection(collection).updateOne(query, newValues);
+}
+
 module.exports = {
   getCount,
   getCountByCity,
+  updateTrip,
+  getTrip,
 }
 
 
--- src/routes/trips/controller.js
+++ src/routes/trips/controller.js
@@ -1,6 +1,7 @@
 /**Controller to manage store of trips endpoint
  * @module routes/trips/controller
  */
+const { ObjectID } = require('mongodb');
 const store = require('../../models/mongodb');
 
 const db="miaguila";
@@ -16,15 +17,39 @@ const collection="trips";
   /**
      * Logic to get the quantity of trips into "trips collection" by city
      * @method GET 
-     * @param {Object} req.body - city: The target city to count its trips.
+     * @param {String} req.query - the target city to count
      * @returns {number} response - quantity of documents into "trips" collection by city
      */
-    async function getQtyTripsByCity(body){
-      return await store.getCountByCity(db,collection, body.query.city);
+    async function getQtyTripsByCity(getQuery){
+      const query = {city:{name:getQuery.city}};
+      return await store.getCountByCity(db,collection, query);
+    }
+      /**
+     * Logic to get a trip by id
+     * @method GET 
+     * @param {Object} req.params - the body of request
+     * @returns {Object} response - trip
+     */
+    async function getTripById(params){
+      const query = {_id:ObjectID(params.id)};
+      return await store.getTrip(db,collection, query);
+  }
+    /**
+     * Logic to update the status of a trip
+     * @method GET 
+     * @param {Object} req.body - the body of request
+     * @returns {Object} response - update accomplished
+     */
+    async function updateStatusTrip(body){
+      const query = {_id:ObjectID(body._id)};
+      const newStatus = { $set: {status: body.status} };
+      return await store.updateTrip(db,collection, query,newStatus);
   }
 
 
   module.exports = {
     getQtyTrips,
     getQtyTripsByCity,
+    updateStatusTrip,
+    getTripById,
   }
