56ceb19bc6bda2fde74a460f9053ecbe04146c6f
wilantury <ventas@antury.net>
2020-06-12 13:30:56 -0500

    [Add] update a trip: start code

--- src/routes/trips/network.js
+++ src/routes/trips/network.js
@@ -10,6 +10,7 @@ const ControllerTrips = require('./controller');
 /**Router */
 router.get('/qty_trips', getQtyTrips);
 router.get('/qty_trips/city', getQtyTripsByCity);
+router.put('/', updateTrip);
 
 /**
  * API Endpoint to get how many trips are into collection "trips" of db.
@@ -35,6 +36,24 @@ async function getQtyTripsByCity(req, res, next){
   try{
       const resGetQtyTripsByCity = await ControllerTrips.getQtyTripsByCity(req);
       response.success(req, res, {quantity:resGetQtyTripsByCity}, 200);
+  }catch(err){
+      response.error(req, res, err.message, 500, 'error network getting QTY of trips by city');
+  }
+}
+
+/**
+ * API Endpoint to update a trip
+ * @method POST 
+ * @param {Object} req - the request object
+ * @returns {Object} - body
+ */
+async function updateTrip(req, res, next){
+  const {option} = req;
+  console.log(option);
+  try{
+      //const resUpdateTrip = await ControllerTrips.updateTrip(req);
+      //response.success(req, res, resUpdateTrip, 200);
+      response.success(req, res, option, 200);
   }catch(err){
       response.error(req, res, err.message, 500, 'error network getting QTY of trips by city');
   }
